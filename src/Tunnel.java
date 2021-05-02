7fed823ee4883a75a995ee557074079de3640e96
Mihail <ptichkinm@mail.ru>
2021-05-02 21:40:47 +0300

    Рабочая версия

--- src/Tunnel.java
+++ src/Tunnel.java
@@ -5,18 +5,19 @@ public class Tunnel extends Stage {
 	}
 	@Override
 	public void go(Car c) {
+
 		try {
-			try {
-				System.out.println(c.getName() + " готовится к этапу(ждет): " + description);
-				System.out.println(c.getName() + " начал этап: " + description);
-				Thread.sleep(length / c.getSpeed() * 1000);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			} finally {
-				System.out.println(c.getName() + " закончил этап: " + description);
-			}
-		} catch (Exception e) {
+			System.out.println(c.getName() + " готовится к этапу(ждет): " + description);
+
+			// В туннель не может заехать одновременно больше половины участников (условность).
+			MainClass.tunnelSemaphore.acquire();
+			System.out.println(c.getName() + " начал этап: " + description);
+			Thread.sleep(length / c.getSpeed() * 1000);
+			System.out.println(c.getName() + " закончил этап: " + description);
+		} catch (InterruptedException e) {
 			e.printStackTrace();
+		} finally {
+			MainClass.tunnelSemaphore.release();
 		}
 	}
 }
